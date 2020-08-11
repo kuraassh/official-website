a7034b853bb4e437833d79b2f34e8fd06ff214b7
Rachel Ho <rachelhoyuenyi@gmail.com>
2020-08-11 21:28:15 +0800

    fix: header component removed

--- src/screens/home/components/mooncake_body/components/header/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { HeaderCSS } from "./styles";
-import { MooncakeDetails, MooncakeImg } from "@components";
-
-const Header = () => {
-  return <HeaderCSS></HeaderCSS>;
-};
-
-export default Header;
