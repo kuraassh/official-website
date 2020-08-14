857659cbcfff30cb506d6155a27218d910948203
Wingman <ryuash123@gmail.com>
2020-08-14 16:58:12 +0800

    whoops

--- src/screens/home/components/news/components/desktop/index.tsx
+++ src/screens/home/components/news/components/desktop/index.tsx
@@ -1,53 +1,93 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
-import { CarouselCSS } from "./styles";
+import { CarouselCSS, TestCSS } from "./styles";
 import { dummyData } from "../carousel_item/config";
 import Post from "../carousel_item/news_posts";
-// import { Next } from "@icons";
+import { MaxWidthContainerCSS } from "@styles/components";
+import { Next } from "@icons";
 
 const { main, blogs } = dummyData;
+
+
+const CustomArrows = ({ onClick, direction }: any) => {
+  return (
+    <TestCSS
+      className={direction}
+      // className="react-multiple-carousel__arrow"
+      onClick={onClick}
+    >
+      <Next />
+    </TestCSS>
+  )
+}
+
+const ButtonGroup = (props: any) => {
+  const {
+    next,
+    previous,
+    carouselState: { currentSlide, totalItems, slidesToShow }
+  } = props;
+
+  return (
+    <div>
+      <TestCSS
+        onClick={() => previous()}><Next /></TestCSS>
+      <TestCSS
+        // className={currentSlide === totalItems - slidesToShow ? "disable" : "react-multiple-carousel__arrow react-multiple-carousel__arrow--right"}
+        onClick={() => next()}
+      >
+        <Next />
+      </TestCSS>
+    </div>
+  );
+};
 
 const CarouselBlogPosts = () => {
   return (
     <CarouselCSS>
-      <Carousel
-        additionalTransfrom={0}
-        arrows
-        autoPlaySpeed={3000}
-        centerMode={false}
-        className=""
-        containerClass="container"
-        //   customLeftArrow={<Next />}
-        //   customRightArrow={<Next />}
-        dotListClass=""
-        draggable
-        focusOnSelect={false}
-        infinite
-        itemClass=""
-        keyBoardControl
-        minimumTouchDrag={80}
-        renderButtonGroupOutside={false}
-        renderDotsOutside={false}
-        responsive={{
-          desktop: {
-            breakpoint: {
-              max: 3000,
-              min: 1024,
+      <MaxWidthContainerCSS>
+        <Carousel
+          additionalTransfrom={0}
+          arrows
+          autoPlaySpeed={3000}
+          centerMode={false}
+          className=""
+          containerClass="container"
+          customLeftArrow={<></>}
+          customRightArrow={<></>}
+          // customLeftArrow={<CustomArrows direction="left" />}
+          // customRightArrow={<CustomArrows direction="right" />}
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          renderButtonGroupOutside
+          customButtonGroup={<ButtonGroup />}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024,
+              },
+              items: 3,
+              partialVisibilityGutter: 40,
             },
-            items: 3,
-            partialVisibilityGutter: 40,
-          },
-        }}
-        showDots={false}
-        sliderClass=""
-        slidesToSlide={1}
-        swipeable
-      >
-        <Post main post={main} />
-        {blogs.map((x, i) => (
-          <Post key={i} post={x} />
-        ))}
-      </Carousel>
+          }}
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={3}
+          swipeable
+        >
+          <Post main post={main} />
+          {blogs.map((x, i) => (
+            <Post key={i} post={x} />
+          ))}
+        </Carousel>
+      </MaxWidthContainerCSS>
     </CarouselCSS>
   );
 };
--- src/screens/home/components/news/components/desktop/styles.tsx
+++ src/screens/home/components/news/components/desktop/styles.tsx
@@ -4,7 +4,46 @@ import { mixins, media } from "@styles";
 export const CarouselCSS = styled.div`
   display: none;
   ${media.bigDesktop`
-   ${mixins.desktopMaxWidth}
    ${mixins.desktopPadding}
+   display: flex;
+   justify-content: center;
+   align-items: center;
+
+  //  .react-multi-carousel-item--active {
+  //     // background: red;
+
+  //     &:first-child {
+  //       background: green;
+  //   }
+
+  //   &:last-child {
+  //       background: yellow;
+  //   }
+  //  }
+
+  .container {
+    // padding: 1rem;
+    // background: yellow;
+  }
   `}
+`;
+
+
+export const TestCSS = styled.span`
+  position: absolute;
+
+  svg {
+    width: 35px;
+    path {
+      // fill: blue;
+    }
+  }
+
+  &.right {
+    right: 0;
+  }
+
+  &.left {
+    left: 0;
+  }
 `;
