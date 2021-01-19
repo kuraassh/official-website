1f1cdc37f387da10823aee46f9098dc005457878
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-01-19 22:14:32 +0800

    clean up

--- src/api/tags/index.ts
+++ src/api/tags/index.ts
@@ -1,18 +1,8 @@
-// import axios from "axios";
 import { ghostApi as api } from "../index";
 // import { IPost } from "./interface";
-// import { tags } from "@tryghost/helpers";
 
 /** Get blog posts by tag */
-export const getPostsByTag = async (
-  query: string
-  // {
-  //   limit = 5,
-  //   page = 1,
-  //   // filter = "tag:-[hash-zhs,hash-zht]",
-  //   filter = `tags:${query}`,
-  // }: IPost
-) => {
+export const getPostsByTag = async (query: string) => {
   try {
     const posts = await api.posts.browse({
       filter: `tags:${query}`,
@@ -21,15 +11,7 @@ export const getPostsByTag = async (
       page: 1,
       formats: "html",
     });
-    // console.log("tag >>>>>>>>>", posts);
     return posts ?? null;
-    // return await api.posts.browse({
-    //   include: "tags,authors",
-    //   limit,
-    //   page,
-    //   filter,
-    //   formats: "html",
-    // });
   } catch (err) {
     console.error(`tag: ${err}`);
     return [];
