b30229e666497cf83cde92ced069e1975e67b18e
razumv <razumv95@gmail.com>
2022-04-05 23:35:44 +0300

    fix

--- src/components/icons/forbole/index.tsx
+++ src/components/icons/forbole/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const POSTCAPITALIST = () => {
+const Forbole = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -24,4 +24,4 @@ const POSTCAPITALIST = () => {
   );
 };
 
-export default POSTCAPITALIST;
+export default Forbole;
