456424100a7ef16ea463549cf9bb1cf77d58cbe0
razumv <razumv95@gmail.com>
2022-04-14 12:51:34 +0300

    update about

--- src/screens/about/components/team/config.tsx
+++ src/screens/about/components/team/config.tsx
@@ -1,7 +1,7 @@
 export const membersData = [
   {
-    name: "Kwun Yeung",
-    position: "cofounderCTO",
+    name: "KURASH",
+    position: "cofounderCEO",
     image: "/static/images/team/kwun.jpg",
     links: {
       linkedin: "https://www.linkedin.com/in/kwunyeung/",
@@ -11,19 +11,19 @@ export const membersData = [
     slug: "kwun",
   },
   {
-    name: "Terence Lam",
-    position: "cofounderCEO",
+    name: "razumv",
+    position: "cofounderCTO",
     image: "/static/images/team/terence-lam.jpg",
     links: {
-      linkedin: "https://www.linkedin.com/in/terencesflam/",
-      github: "https://github.com/terencesflam",
-      twitter: "https://twitter.com/terencesflam",
+      linkedin: "www.linkedin.com/in/razumv",
+      github: "https://github.com/razumv",
+      twitter: "https://twitter.com/razumv1",
     },
     slug: "terence",
   },
   {
-    name: "Shilin Zheng",
-    position: "directorParternship",
+    name: "Godshunter",
+    position: "directorProductMkt",
     image: "/static/images/team/shilin.jpeg",
     links: {
       linkedin: "https://www.linkedin.com/in/ruoyin-zheng-b6687a32/",
@@ -32,8 +32,8 @@ export const membersData = [
     slug: "shilin",
   },
   {
-    name: "Michael Wu",
-    position: "directorParternship",
+    name: "IceFrosTv2",
+    position: "asistProductMkt",
     image: "/static/images/team/michael.jpg",
     links: {
       linkedin: "https://www.linkedin.com/in/michael-wu-a959123b",
@@ -42,8 +42,8 @@ export const membersData = [
     slug: "michael",
   },
   {
-    name: "Riccardo Montagnin",
-    position: "techLead",
+    name: "Twist",
+    position: "DevOpsEng",
     image: "/static/images/team/riccardo.jpg",
     links: {
       linkedin: "https://www.linkedin.com/in/riccardomontagnin/",
@@ -53,309 +53,13 @@ export const membersData = [
     slug: "riccardo",
   },
   {
-    name: "Muhammad Elsayeh",
-    position: "blockchainEngineer",
+    name: "xDMK",
+    position: "DevOpsEng",
     image: "/static/images/team/muhammad.jpg",
     links: {
       linkedin: "https://www.linkedin.com/in/muhammad-elsayeh-ph-d-76b52529/",
       github: "https://github.com/Sayeh-1337",
-      // twitter: "https://twitter.com/Lorinaura?s=20",
     },
     slug: "muhammad",
   },
-  {
-    name: "Ravi Soni",
-    position: "infrastructureLead",
-    image: "/static/images/team/ravi.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/rav-s/",
-    },
-    slug: "ravi",
-  },
-  {
-    name: "Charles Pusey",
-    position: "infrastructureEngineer",
-    image: "/static/images/team/charles.jpeg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/chccp/",
-      twitter: "https://twitter.com/CCrypto19961035",
-    },
-    slug: "charles",
-  },
-  {
-    name: "Hiep Mai",
-    position: "trainDevOpsEng",
-    image: "/static/images/team/hiep.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/quanghiep-mai/",
-      twitter: "https://twitter.com/hiepmq",
-    },
-    slug: "hiep",
-  },
-  {
-    name: "Calvin Kei",
-    position: "leadFrontDev",
-    image: "/static/images/team/calvin.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/calvinkei/",
-      github: "https://github.com/calvinkei",
-    },
-    slug: "calvin",
-  },
-  {
-    name: "Wingman Lee",
-    position: "sswe",
-    image: "/static/images/team/wingman.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/wingman-lee/",
-      github: "https://github.com/ryuash",
-    },
-    slug: "wingman",
-  },
-  {
-    name: "Kevin Pang",
-    position: "sswe",
-    image: "/static/images/team/kevin.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/kppang/",
-      github: "https://github.com/Moistbobo",
-    },
-    slug: "kevin",
-  },
-  {
-    name: "Leonardo Bragagnolo",
-    position: "sswe",
-    image: "/static/images/team/leo.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/leonardo-bragagnolo-9a2ab4186/",
-      github: "https://github.com/bragaz",
-      twitter: "https://twitter.com/leobragaz_",
-    },
-    slug: "leo",
-  },
-  {
-    name: "Manuel Turetta ",
-    position: "sswe",
-    image: "/static/images/team/manuel.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/manuel-turetta-9b3272160/",
-      twitter: "https://twitter.com/TurettaManuel",
-      github: "https://github.com/manu0466",
-    },
-    slug: "manuel",
-  },
-  {
-    name: "Alessandro Mazzon",
-    position: "swe",
-    image: "/static/images/team/alessandro.jpeg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/ale-mazzon/",
-      github: "https://github.com/ale-mazz",
-      twitter: "https://twitter.com/ale_mazzon",
-    },
-    slug: "rachel",
-  },
-  {
-    name: "Rachel Ho",
-    position: "swe",
-    image: "/static/images/team/rachel.jpg",
-    links: {
-      linkedin: "http://linkedin.com/in/rachel-ho-1016",
-      github: "https://github.com/rachelhox/",
-      twitter: "https://twitter.com/zadpoke",
-    },
-    slug: "rachel",
-  },
-  {
-    name: "Monika Pusz",
-    position: "swe",
-    image: "/static/images/team/monika.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/monika-pusz-b90906150/",
-      github: "https://github.com/MonikaCat",
-    },
-    slug: "monika",
-  },
-  {
-    name: "Apple Choi",
-    position: "swe",
-    image: "/static/images/team/apple.jpeg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/hau-yi-choi-4929b6153/",
-    },
-    slug: "apple",
-  },
-  {
-    name: "Paul Chen",
-    position: "swe",
-    image: "/static/images/team/paul.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/lin-yang-chen-566b23192/",
-      github: "https://github.com/dadamu",
-    },
-    slug: "paul",
-  },
-  {
-    name: "Franco Chau",
-    position: "swe",
-    image: "/static/images/team/franco.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/franco-chau-96411317b",
-      github: "https://github.com/francochau",
-    },
-    slug: "franco",
-  },
-  {
-    name: "Aaron Tsou",
-    position: "swe",
-    image: "/static/images/team/aaron.jpg",
-    links: {
-      twitter: "https://twitter.com/AaronTsou",
-      linkedin: "https://www.linkedin.com/in/atsou/",
-      github: "https://github.com/huichiaotsou",
-    },
-    slug: "aaron",
-  },
-  {
-    name: "Ellie Yang",
-    position: "swe",
-    image: "/static/images/team/ellie.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/ellie-y/",
-      github: "https://github.com/yayay927",
-    },
-    slug: "ellie",
-  },
-  {
-    name: "Caroline Lau",
-    position: "communityManager",
-    image: "/static/images/team/caroline.jpeg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/caroline-lauhy/",
-      twitter: "https://twitter.com/carolinelauhy",
-    },
-    slug: "caroline",
-  },
-  {
-    name: "Laura Nori",
-    position: "communityManager",
-    image: "/static/images/team/laura-nori.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/laura-nori-67826015a/",
-      twitter: "https://twitter.com/Lorinaura?s=20",
-    },
-    slug: "laura",
-  },
-  {
-    name: "Rooty Chan",
-    position: "seniorUXDesigner",
-    image: "/static/images/team/rooty.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/rootychan/",
-    },
-    slug: "rooty",
-  },
-  {
-    name: "Viola Chan",
-    position: "seniorUXDesigner",
-    image: "/static/images/team/viola.jpg",
-    links: {
-      twitter: "https://twitter.com/Viocmm",
-      linkedin: "https://www.linkedin.com/in/violacc/",
-    },
-    slug: "viola",
-  },
-  {
-    name: "Nicole To",
-    position: "uiuxDesigner",
-    image: "/static/images/team/nicole.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/wan-ting-nicole-to-ab9314215",
-    },
-    slug: "nicole",
-  },
-  {
-    name: "Vivian Wong",
-    position: "uiuxDesigner",
-    image: "/static/images/team/vivian.jpeg",
-    links: {
-      twitter: "https://twitter.com/vivianw271",
-    },
-    slug: "vivian",
-  },
-  {
-    name: "Carine Cho",
-    position: "stratManager",
-    image: "/static/images/team/carine.png",
-    links: {
-      linkedin: "https://www.linkedin.com/in/carine-cho",
-    },
-    slug: "carine",
-  },
-  {
-    name: "Jade Lai",
-    position: "directorProductMkt",
-    image: "/static/images/team/jade.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/jade-lai-18543432",
-      twitter: "https://twitter.com/jadelaiwy",
-    },
-    slug: "jade",
-  },
-  {
-    name: "Brian Chan",
-    position: "talentAqt",
-    image: "/static/images/team/brian.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/brian-chan-bhn/",
-      twitter: "https://twitter.com/brianchan_bhn",
-    },
-    slug: "brian",
-  },
-  {
-    name: "Ron Roy",
-    position: "prodLead",
-    image: "/static/images/team/ron.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/ronroyhk/",
-    },
-    slug: "ron",
-  },
-  {
-    name: "Wei Ly",
-    position: "prodLead",
-    image: "/static/images/team/wei.jpeg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/wei-ly-81371b3/",
-      twitter: "https://twitter.com/ly_wei",
-      medium: "https://weiking.medium.com/",
-    },
-    slug: "wei",
-  },
-  {
-    name: "Kannie Chu",
-    position: "projManager",
-    image: "/static/images/team/kannie.jpg",
-    links: {
-      linkedin: "https://www.linkedin.com/in/kannie-chu/",
-      twitter: "https://twitter.com/kachujai",
-      github: "https://github.com/kanniec826",
-    },
-    slug: "kannie",
-  },
-  {
-    name: "Christy Li",
-    position: "exeAssitant",
-    image: "/static/images/team/christy.webp",
-    links: {
-      linkedin: "https://www.linkedin.com/in/christywyli/",
-      twitter: "https://twitter.com/christyli_fb",
-    },
-    slug: "christy",
-  },
-  // {
-  // name: "You",
-  // position: "researchContent",
-  // image: "images/team/placeholder.png",
-  // },
 ];
